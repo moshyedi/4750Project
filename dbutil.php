6930daec920111c29175bbaabf188d714f168d71
rjcoulter <rjc8ez@virginia.edu>
2019-10-28 16:59:06 -0400

    Add ability to select and insert from ALBUM table

--- dbutil.php
+++ dbutil.php
@@ -1,21 +1,19 @@
 <?php
 class DbUtil{
-      public static $loginUser = "acm4xb"; 
-      public static $loginPass = "#Acm1030";
-      public static $host = "cs4750.cs.virginia.edu"; // DB Host
-      public static $schema = "acm4xb"; // DB Schema
+	public static $loginUser = "rjc8ez"; 
+	public static $loginPass = "ryan22";
+	public static $host = "cs4750.cs.virginia.edu"; // DB Host
+	public static $schema = "rjc8ez_Project"; // DB Schema
 
-      public static function loginConnection(){
-      	     	    $db = new mysqli(DbUtil::$host, DbUtil::$loginUser, DbUtil::$loginPass, DbUtil::$schema);
+	public static function loginConnection(){
+	$db = new mysqli(DbUtil::$host, DbUtil::$loginUser, DbUtil::$loginPass, DbUtil::$schema);
 
-		    	  if($db->connect_errno){
-							echo("Could not connect to db");
-								    		   $db->close();
-														exit();
-															 }
-
-															   return $db;
-															   }
-
+	if($db->connect_errno){
+		echo("Could not connect to db");
+			$db->close();
+			exit();
+	}
+	return $db;
+	}
 }
 ?>
