c04811402c421e845b080219af22e2c7423b62a2
hdc2br <42917065+hdc2br@users.noreply.github.com>
2019-11-12 17:33:22 -0500

    Update AlbumSelect.php

--- AlbumSelect.php
+++ AlbumSelect.php
@@ -14,11 +14,14 @@
     $result = mysqli_query($con, $sql);
 
     // Print the data from the table row by row
+    echo '<table style="width:100%"><tr><th>Album ID</th><th>Title</th><th>Release Date</th></tr>';
     while($row = mysqli_fetch_array($result)) {
-        echo $row['ALBUM_ID'];
-        echo " " . $row['ALBUM_TITLE'];
-        echo " " . $row['RELEASE_DATE'];
-        echo "<br>";
+	echo "<tr>";
+        echo "<td>" . $row['ALBUM_ID'] . "</td>";
+        echo "<td>" . $row['ALBUM_TITLE'] . "</td>";
+        echo "<td>" . $row['RELEASE_DATE'] . "</td>";
+        echo "</tr>";
     }
+    echo '</table>';
     mysqli_close($con);
  ?>
